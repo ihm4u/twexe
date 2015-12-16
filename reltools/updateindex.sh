2029dc2ce1fcc9ab9c399d03b5e120b566a87279
ihm4u <ihm4u>
2015-12-15 20:56:41 -0500

    Text index

--- reltools/updateindex.sh
+++ reltools/updateindex.sh
@@ -11,5 +11,5 @@ if [ -z "$1" ]; then
    echo Please specify wiki file to update.
    exit 1;
 fi
-perl -pi -e 's#(<div.*?title="Version">\s*<pre>).*?(</pre>\s*</div>)#${1}'$VERSION'${2}#s' "$1"
+perl -0777 -pi -e 's#(<div.*?title="Version">\s*<pre>).*?(</pre>\s*</div>)#${1}'$VERSION'${2}#s' "$1"
 echo Updated "$1" to version $VERSION.
